6f73238f341a72dd68d05f3207dbcea5eab654fb
Pablo <pablodiegodemuner@gmail.com>
2021-01-10 10:26:53 -0400

    Add Search order location Page

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -6,25 +6,30 @@ import React, { useEffect, useState } from 'react';
 import { fetchProducts } from '../api';
 import ProductsList from './ProductsList';
 import StepsHeader from './StepsHeader';
-import {Product} from './types';
+import { OrderLocationdata, Product } from './types';
 import './styles.css';
+import OrderLocation from './OrderLocation';
 
 
 function Orders() {
 
-const [products, setProducts] = useState <Product[]> ([]);
-  
-    useEffect(()  => {
-      fetchProducts()
+  const [products, setProducts] = useState<Product[]>([]);
+
+  const [orderLocation, setOrderLocation] = useState<OrderLocationdata>();
+
+  useEffect(() => {
+    fetchProducts()
       .then(response => setProducts(response.data))
-      .catch(error => console.error(error))  
-    }, [] );
-    
-    return(
-       <div className="orders-container">
-           <StepsHeader/>
-           <ProductsList products = {products}/>
-       </div> 
+      .catch(error => console.error(error))
+  }, []);
+
+  return (
+    <div className="orders-container">
+      <StepsHeader />
+      <ProductsList products={products} />
+      <OrderLocation onChangeLocation={location => setOrderLocation(location)} />
+      </div>
+       
     )
 }
 export default Orders;
