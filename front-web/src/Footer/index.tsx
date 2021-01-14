e56278272aeead6ef11aded91c55f8e4e84d50d0
Pablo <pablodiegodemuner@gmail.com>
2021-01-14 00:22:29 -0400

    Config Front

--- front-web/src/Footer/index.tsx
+++ front-web/src/Footer/index.tsx
@@ -1,24 +1,16 @@
 /* Rodapé da página*/ 
 
 import './styles.css';
-import {ReactComponent as YouTubeIcon} from './youtube.svg';
-import {ReactComponent as LinkedInIcon} from './linkedin.svg';
-//import {ReactComponent as InstagramIcon} from './instagram.svg';
-import {ReactComponent as FacebookIcon} from './Facebook.svg';
+
+import {ReactComponent as InstagramIcon} from './instagram.svg';
 
 function Footer() {
     return(
         <footer className="main-footer">
-       Siga-nos em nossas redes sociais.
-       
-         <a href="https://www.youtube.com/results?search_query=dev+superior" target= "new">
-                <YouTubeIcon/>
-             </a>
-             <a href="https://www.linkedin.com/in/pablo-diego-demuner-costa-31695b1b7/" target= "new">
-                <LinkedInIcon/>
-             </a>   
-             <a href="https://www.facebook.com/pablodiego.demunercosta" target= "new">
-                <FacebookIcon/>
+       Sigam nossas redes sociais.
+        
+             <a href="http://" target= "new">
+                <InstagramIcon/>
              </a>
             
         </footer>
